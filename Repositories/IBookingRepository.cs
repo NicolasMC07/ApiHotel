7ad21a67797a116029f0e308d6e8a6f70f41b6e5
NicolasMC07 <nicolasmarcar2006@gmail.com>
2024-10-07 08:42:20 -0500

    feat: creting repositories folder and the shell of the interfaces

---
+++ Repositories/IBookingRepository.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ApiHotel.Models;
+
+namespace ApiHotel.Repositories
+{
+    public interface IBookingRepository
+    {
+        
+    }
+}
